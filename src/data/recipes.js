586e705fd19fd92714b61fea85d266cf7644ecdd
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-10 22:52:38 +1000

    feat: :monocle_face: add data offline in local

---
+++ src/data/recipes.js
@@ -0,0 +1,17 @@
+export const recipes = [
+	{
+		id: 'arroz-leche',
+		title: 'Arroz de leche',
+		image: '/recipes/recipe_arroz_de_leche.webp'
+	},
+	{
+		id: 'arroz-frito',
+		title: 'Arroz frito',
+		image: '/recipes/recipe_arroz_frito.webp'
+	},
+	{
+		id: 'pasta-carbonara',
+		title: 'Pasta carbonara',
+		image: '/recipes/recipe_pasta_carbonara.webp'
+	}
+]
