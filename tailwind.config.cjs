8116d5483359b5bc4f06573a90b219c76d8d8763
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-10 22:51:18 +1000

    chore: :bento: add files local of fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+		}
 	},
-	plugins: [],
+	plugins: []
 }
